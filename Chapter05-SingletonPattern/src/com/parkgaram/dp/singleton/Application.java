341f3300b4be0a862165373c7c6ee5fbe382c16a
hh.yoon <hh.yoon@lge.com>
2021-04-05 10:47:02 +0900

    singleton pattern

--- Chapter05-SingletonPattern/src/com/parkgaram/dp/singleton/Application.java
+++ Chapter05-SingletonPattern/src/com/parkgaram/dp/singleton/Application.java
@@ -3,8 +3,19 @@ package com.parkgaram.dp.singleton;
 public class Application {
 
 	public static void main(String[] args) {
-		SomeSystemComponent component = SomeSystemComponent.getInstance();
-		
+		SomeSystemComponent component1 = SomeSystemComponent.getInstance();
+		SomeSystemComponent component2 = SomeSystemComponent.getInstance();
+
+		System.out.println(component1.getConf1());
+		System.out.println(component2.getConf1());
+
+		component1.setConf1(11);
+		System.out.println(component1.getConf1());
+		System.out.println(component2.getConf1());
+
+		component2.setConf1(22);
+		System.out.println(component1.getConf1());
+		System.out.println(component2.getConf1());
 	}
 
 }
--- Chapter05-SingletonPattern/src/com/parkgaram/dp/singleton/SomeSystemComponent.java
+++ Chapter05-SingletonPattern/src/com/parkgaram/dp/singleton/SomeSystemComponent.java
@@ -2,18 +2,29 @@ package com.parkgaram.dp.singleton;
 
 public class SomeSystemComponent {
 
+	// 하나만 있어야 하므로 static
 	private static SomeSystemComponent instance;
+	private int somesetting1;
 	
 	private SomeSystemComponent() {
-		
+		somesetting1 = 5;
 	}
 	
 	public static SomeSystemComponent getInstance(){
 		if (instance == null) {
-			
-		}else {
+			System.out.println("신규 생성");
 			instance = new SomeSystemComponent();
+		}else {
+			System.out.println("생성되어 있음.");
 		}
 		return instance;
+	}
+
+	public int getConf1() {
+		return somesetting1;
+	}
+
+	public void setConf1(int set1) {
+		somesetting1 = set1;
 	}
 }
